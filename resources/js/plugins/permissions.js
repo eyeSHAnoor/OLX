ef806cd744aadec717ff0ab99dd0c9f1dfd61c6b
eyeSHAnoor <www.prisha7@gmail.com>
2026-02-26 15:35:55 +0500

    changes

--- resources/js/plugins/permissions.js
+++ resources/js/plugins/permissions.js
@@ -1,9 +1,9 @@
-import usePermissions, { hasPermissions } from '../Composables/usePermissions';
+import usePermissions from '../composables/usePermissions';
 
 export default {
     install(app) {
-        const permissions = usePermissions()
-        app.config.globalProperties.hasRole = permissions.hasRole
-        app.config.globalProperties.hasPermissions = permissions.hasPermissions
-    }
-}
+        const permissions = usePermissions();
+        app.config.globalProperties.hasRole = permissions.hasRole;
+        app.config.globalProperties.hasPermissions = permissions.hasPermissions;
+    },
+};
