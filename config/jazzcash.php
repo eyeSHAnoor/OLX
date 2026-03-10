af9f33e398a6e3a7f1e06852e2c3735e84a0384d
eyeSHAnoor <www.prisha7@gmail.com>
2026-03-10 20:15:52 +0500

    changes

--- config/jazzcash.php
+++ config/jazzcash.php
@@ -1,20 +1,68 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | Payment Mode
+    |--------------------------------------------------------------------------
+    |
+    | This value determines whether you are in sandbox (testing) or production (live) mode.
+    | Accepted values: 'sandbox' or 'production'
+    |
+    */
+    'paymentmode' => env('JAZZCASH_PAYMENTMODE', 'sandbox'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Merchant Credentials
+    |--------------------------------------------------------------------------
+    |
+    | These values are provided by JazzCash when you register as a merchant.
+    |
+    */
     'merchant_id' => env('JAZZCASH_MERCHANT_ID', ''),
     'password' => env('JAZZCASH_PASSWORD', ''),
-    'integerity_salt' => env('JAZZCASH_INTEGERITY_SALT', ''),
-    
-    'return_url' => env('JAZZCASH_RETURN_URL', '/jazzcash/callback'),
-    'ipn_url' => env('JAZZCASH_IPN_URL', '/jazzcash/ipn'),
-    
-    'environment' => env('JAZZCASH_ENVIRONMENT', 'sandbox'),
-    
-    'endpoints' => [
-        'sandbox' => 'https://sandbox.jazzcash.com.pk/CustomerPortal/transactionmanagement/merchantform/',
-        'production' => 'https://payments.jazzcash.com.pk/CustomerPortal/transactionmanagement/merchantform/'
-    ],
-    
-    'currency' => 'PKR',
+    'hash_key' => env('JAZZCASH_HASHKEY', ''), // Also called integrity salt
+    'mpin' => env('JAZZCASH_MPIN', ''),
+
+    /*
+    |--------------------------------------------------------------------------
+    | API URLs
+    |--------------------------------------------------------------------------
+    |
+    | The endpoints for JazzCash API in different environments.
+    |
+    */
+    'production_url' => env('JAZZCASH_PRODUCTION_URL', 'https://payments.jazzcash.com.pk/CustomerPortal/transactionmanagement/merchantform/'),
+    'sandbox_url' => env('JAZZCASH_SANDBOX_URL', 'https://sandbox.jazzcash.com.pk/CustomerPortal/transactionmanagement/merchantform/'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Return URL
+    |--------------------------------------------------------------------------
+    |
+    | The URL where JazzCash will redirect the customer after payment.
+    |
+    */
+    'return_url' => env('JAZZCASH_RETURNURL', ''),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Currency Code
+    |--------------------------------------------------------------------------
+    |
+    | Default currency code for transactions (PKR for Pakistan Rupee).
+    |
+    */
+    'currency_code' => 'PKR',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Language
+    |--------------------------------------------------------------------------
+    |
+    | Default language for payment page (EN for English, UR for Urdu).
+    |
+    */
     'language' => 'EN',
 ];
