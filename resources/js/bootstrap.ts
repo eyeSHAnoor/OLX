6bfecb469260302e2bad7e9c57ee0535dc017d9c
eyeSHAnoor <www.prisha7@gmail.com>
2026-04-02 22:02:20 +0500

    changes in bootstrap

--- resources/js/bootstrap.ts
+++ resources/js/bootstrap.ts
@@ -1,3 +1,19 @@
+// import Echo from 'laravel-echo';
+// import Pusher from 'pusher-js';
+
+// window.Pusher = Pusher;
+
+// window.Echo = new Echo({
+//     broadcaster: 'reverb',
+//     key: import.meta.env.VITE_REVERB_APP_KEY,
+//     wsHost: import.meta.env.VITE_REVERB_HOST ?? window.location.hostname,
+//     wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+//     wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
+//     forceTLS: false,
+//     disableStats: true,
+//     enabledTransports: ['ws', 'wss'],
+// });
+
 import Echo from 'laravel-echo';
 import Pusher from 'pusher-js';
 
@@ -6,10 +22,10 @@ window.Pusher = Pusher;
 window.Echo = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST ?? window.location.hostname,
-    wsPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    wssPort: import.meta.env.VITE_REVERB_PORT ?? 8080,
-    forceTLS: false,
+    wsHost: import.meta.env.VITE_REVERB_HOST,
+    wsPort: import.meta.env.VITE_REVERB_PORT,
+    wssPort: import.meta.env.VITE_REVERB_PORT,
+    forceTLS: import.meta.env.VITE_REVERB_SCHEME === 'https',
     disableStats: true,
     enabledTransports: ['ws', 'wss'],
 });
